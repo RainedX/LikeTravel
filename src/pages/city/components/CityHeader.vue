fba63680f59067a03ce762029643d12f83bb15ce
yr49392 <yr49392@ly.com>
2018-08-17 11:02:25 +0800

    新增城市页面

---
+++ src/pages/city/components/CityHeader.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="header">
+        <router-link to="/">
+            <div class="header-left">
+                <i class="iconfont icon-fanhui"></i>
+            </div>
+        </router-link>
+        <div class="header-content">
+            城市选择
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data () {
+        return {
+        };
+    }
+}
+
+</script>
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl';
+    .header
+        display flex
+        color #fff
+        line-height .86rem
+        background $bgColor
+        .header-left
+            color #fff
+            float left
+            width .64rem
+            .icon-fanhui
+                display block
+                font-size .4rem
+                text-align center 
+        .header-content
+            position absolute
+            left 50%
+            transform translateX(-50%)
+            line-height .86rem
+            font-size 0.35rem
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="header">
         <div class="header-left">
-            <i class="iconfont icon-fanhui"></i>
+            首页
         </div>
         <div class="header-input">
             <i class="iconfont icon-sousuo"></i> 输入城市/景点/游玩主题
         </div>
-        <div class="header-right">{{city}}<i class="iconfont icon-jiantou"></i></div>
+        <router-link to="/city">
+            <div class="header-right">{{city}}<i class="iconfont icon-jiantou"></i></div>
+        </router-link>
     </div>
 </template>
 
@@ -31,12 +33,9 @@ export default {
         line-height .86rem
         background $bgColor
         .header-left
-            float left
+            padding-left .2rem
+            line-height .92rem
             width .64rem
-            .icon-fanhui
-                display block
-                font-size .4rem
-                text-align center 
         .header-input
             flex 1
             line-height .64rem
@@ -47,6 +46,7 @@ export default {
             .icon-sousuo
                 margin-left .05rem
         .header-right
+            color #fff
             float right
             width 1.24rem 
             text-align center
